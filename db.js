23134150100b40541720353a9b22f49ed79c2f1c
SIRAPARAPU SAI SATYA PAVAN CHARAN <144811045+pavan-charan@users.noreply.github.com>
2025-02-27 23:19:42 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',  // Replace with your database host
-    user: 'root',       // Your MySQL username
-    password: 'Pavan@123',  // Your MySQL password
-    database: 'projectdb', // Your MySQL database name
+    host: process.env.HOST, 
+    user: process.env.USER,       
+    password: process.env.PASSWORD,  
+    database: process.env.DATABASE, 
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0
